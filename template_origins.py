0065398e88b5d3de60d6a11855363731bd6f559c
aero-DS <33962783+rookieML-IN@users.noreply.github.com>
2024-11-17 03:33:55 +0530

    [Modified] - template_origins.py 1. Refactored to make the code dynamic.
    [Added]
    1. Site_Names.json
    2. Templates_Names.json

--- template_origins.py
+++ template_origins.py
@@ -1,49 +1,69 @@
-import argparse
 import os
 import shutil
+import json
+import argparse
 import process_file as pf
 
-# Instantiate
-parser = argparse.ArgumentParser()
+# Dealing with JSONS
+with open('Site_Names.json', 'r') as abb_file:
+    abb_dict = json.load(abb_file)
 
-# Arguments
-parser.add_argument('site_name', choices=['MH', 'ZN', 'KG', 'AI', 'AV'],
-                    help='Name of the Website containing the Project')
-parser.add_argument('working_directory', help='Name of the Project Folder')
-parser.add_argument('problem_type', type=int, help='1- Tabular Regression, 2- Tabular Classification')
+with open('Templates_Names.json', 'r') as temp_file:
+    templates_dict = json.load(temp_file)
+
+# Instantiate
+parser = argparse.ArgumentParser(description="Setting-up the Directory for Different Projects")
+
+# Positional Arguments
+parser.add_argument("site_name_abbv", help="Name of the Site containing the project")
+parser.add_argument("working_dir_name", help="Name of the Case-Specific Directory")
+parser.add_argument('problem_type', type=int,
+                    help='1- Tabular Regression, 2- Tabular Classification')
 
 # Parsing
 args = parser.parse_args()
 
-# Abbreviation Dictionary
-abb_dict = {'MH': "MachineHack",
-            'ZN': "Zindi",
-            'KG': "Kaggle",
-            'AI': "AI_Crowd",
-            'AV': "Analytics Vidya"}
 
-# Working Directory Creation
-path = os.path.join(pf.read_config(sec='Base_Directories', ky='hackathon_parent_dir'),
-                    abb_dict.get(args.site_name), args.working_directory)
+# Supplementary Functions
+def create_parent_dir(user_input):
+    par_dir = os.path.join(pf.read_config(sec='Base_Directories', ky='hackathon_parent_dir'), user_input)
+    os.mkdir(par_dir)
 
-os.mkdir(path)
-return_to_previous_state = 0
 
-# Template Copying into the Working Directory
-try:
-    if args.problem_type == 2:
-        file_source = pf.read_config(sec='Templates', ky='tabular_classification_file')
-        file_dest = str(path)+"\\"
-        shutil.copy2(file_source, file_dest)
+def create_working_directory(temp_dict, working_directory):
+    work_dir = os.path.join(pf.read_config(sec='Base_Directories', ky='hackathon_parent_dir'),
+                            temp_dict.get(args.site_name_abbv), working_directory)
+    os.mkdir(work_dir)
 
-except PermissionError:
-    return_to_previous_state = 1
-    print("Template File has NO required permissions to be copied onto the Project Working Directory")
 
-finally:
-    if return_to_previous_state == 1:
-        os.rmdir(path)
-        print("The Newly Created Directory has been DELETED due to exception.")
+def copy_template(temp_dict, template_type, file_dest):
+    global temp_file_ky
+    for k, v in temp_dict.items():
+        if v == template_type:
+            temp_file_ky = k
+    file_src = pf.read_config(sec='Templates', ky=temp_file_ky)
+    shutil.copy2(file_src, file_dest)
 
-    else:
-        print("Working Directory has been created and the relevant template has been copied successfully.")
+
+# Working Code
+if __name__ == "__main__":
+    if str(args.site_name_abbv).upper() not in abb_dict.keys():
+        v_input = str(input("Enter the full name of the Site: "))
+        abb_dict.update({str(args.site_name_abbv).upper(): v_input})
+
+        # Create New Parent Directory
+        create_parent_dir(user_input=v_input)
+        print("New Parent Directory has been created")
+
+        with open('Site_Names.json', 'w') as site:
+            json.dump(abb_dict, site)
+        print("JSON file has been updated with the newly created site-name")
+
+    try:
+        create_working_directory(temp_dict=abb_dict, working_directory=args.working_dir_name)
+        copy_template(temp_dict=templates_dict, template_type=args.problem_type,
+                      file_dest=str(os.path.join(pf.read_config(sec='Base_Directories', ky='hackathon_parent_dir'),
+                                                 abb_dict.get(args.site_name_abbv), args.working_dir_name))+"\\")
+
+    except PermissionError:
+        print("Template File has NO required permissions to be copied onto the Project Working Directory")
